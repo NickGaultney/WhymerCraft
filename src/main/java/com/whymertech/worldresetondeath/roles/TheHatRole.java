d089cf533e0949109ed1275c5a42a692fca71427
Nick Gaultney <nmgaultney@gmail.com>
2024-08-09 21:07:16 -0500

    Adjust Player Respawn

--- src/main/java/com/whymertech/worldresetondeath/roles/TheHatRole.java
+++ src/main/java/com/whymertech/worldresetondeath/roles/TheHatRole.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import com.whymertech.worldresetondeath.GameManager;
+import com.whymertech.worldresetondeath.Plugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -25,9 +26,9 @@ public class TheHatRole extends GenericRole {
 
     public double health = 20.0;
     public Location island;
-    private int xOffset = -1;
-    private int yOffset = 4;
-    private int zOffset = 4;
+    private double xOffset = 1.5;
+    private double yOffset = 4;
+    private double zOffset = 3.5;
 
     public TheHatRole(Player player) {
         super(player);
@@ -51,19 +52,24 @@ public class TheHatRole extends GenericRole {
     public void preparePlayer() {
         File skyblockFile = new File(plugin.getDataFolder(), "skyblock.nbt");
         World world = Bukkit.getWorld(GameManager.WORLD_NAME);
+        Plugin plugin = (Plugin) Bukkit.getPluginManager().getPlugin("worldresetondeath");
+
         try {
             island = new Location(world, randInt(-100, 100), 250, randInt(-100, 100));
+            plugin.getLogger().info("Skyblock Location Set");
             Structure skyblock = Bukkit.getStructureManager().loadStructure(skyblockFile);
             //Random orientation is cool, but save until i can calculate correct spawn location
 //            skyblock.place(island, false, StructureRotation.values()[randInt(0, 3)], Mirror.values()[randInt(0, 2)], 0, 1, new Random());
             skyblock.place(island, false, StructureRotation.NONE, Mirror.NONE, 0, 1, new Random());
-            giveItems();
+            plugin.getLogger().info("Skyblock has been placed");
+            giveLimitedItems();
         } catch (IOException e) {
-            giveFreeElytra();
+            giveItems();
             island = new Location(world,
                     world.getSpawnLocation().getBlockX() - xOffset,
                     world.getSpawnLocation().getBlockY() - yOffset,
                     world.getSpawnLocation().getBlockZ() - zOffset);
+            plugin.getLogger().severe("Failed to generate skyblock: " + e.getMessage());
         }
     }
 
@@ -77,23 +83,23 @@ public class TheHatRole extends GenericRole {
         return "TheHat";
     }
 
-    public void giveFreeElytra() {
+    public void giveItems() {
+        giveBaseSword();
+        giveBasePickaxe();
+        giveBaseAxe();
+        giveBaseShovel();
+        
         ItemStack elytra = new ItemStack(Material.ELYTRA);
         ItemStack rockets = new ItemStack(Material.FIREWORK_ROCKET, 64);
 
         super.enchantItem(Enchantment.MENDING, 1, elytra);
         super.enchantItem(Enchantment.UNBREAKING, 255, elytra);
 
-        giveItems();
-
         super.player.getInventory().addItem(elytra, rockets);
-
     }
 
-    public void giveItems() {
-        giveBaseSword();
-        giveBasePickaxe();
-        giveBaseAxe();
-        giveBaseShovel();
+    public void giveLimitedItems() {
+        ItemStack pickaxe = new ItemStack(Material.STONE_PICKAXE);
+        super.player.getInventory().addItem(pickaxe);
     }
 }
