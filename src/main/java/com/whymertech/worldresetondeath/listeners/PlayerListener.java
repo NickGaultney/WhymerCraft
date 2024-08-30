da694bc8f6eff93698efbadff85cc26f8774fd1c
Nick Gaultney <nmgaultney@gmail.com>
2024-08-30 10:14:07 -0500

    Remove unused libraries

--- src/main/java/com/whymertech/worldresetondeath/listeners/PlayerListener.java
+++ src/main/java/com/whymertech/worldresetondeath/listeners/PlayerListener.java
@@ -3,12 +3,7 @@ package com.whymertech.worldresetondeath.listeners;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
-import com.whymertech.worldresetondeath.roles.Role;
-import com.whymertech.worldresetondeath.roles.BlackSmithRole;
 
 import com.whymertech.worldresetondeath.GameManager;
 
